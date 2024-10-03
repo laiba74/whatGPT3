165eaf82bce372dd95189ce59ded7a4692cca1e3
laiba waseem <laibawasim74@gmail.com>
2024-10-03 20:04:29 +0500

    possibility section added

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -27,9 +27,10 @@ const Header = () => {
           <img src={people} alt="people" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        <div className="gpt3__header__image">
-          <img src={ai} alt="AI Image" />
-        </div>
+      </div>
+
+      <div className="gpt3__header-image">
+        <img src={ai} alt="AI" />
       </div>
     </div>
   );
